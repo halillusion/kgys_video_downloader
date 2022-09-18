a5ae27eae70b9afc0c0366d2d5ee637b67a4bacb
halillusion <halillusion@gmail.com>
2022-09-19 00:12:22 +0300

    latest

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '16ef7467d570d8012a7136ebf471e81601a3e3d9',
+        'name' => 'halillusion/kgys_video_downloader',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'halillusion/kgys_video_downloader' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '16ef7467d570d8012a7136ebf471e81601a3e3d9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
